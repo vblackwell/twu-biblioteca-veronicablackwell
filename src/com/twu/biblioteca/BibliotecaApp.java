134f4ba98a668cc11d0949afc914c1ee947f8b20
Veronica Blackwell <veronicablackwell@Veronicas-MacBook-Pro-20.local>
2019-08-19 11:25:28 -0500

    working on 1.10

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,5 +1,8 @@
 package com.twu.biblioteca;
 
+import org.mockito.internal.configuration.injection.scanner.MockScanner;
+
+import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.Arrays;
 import java.util.List;
@@ -7,29 +10,82 @@ import java.util.List;
 public class BibliotecaApp {
 
     protected List<Book> list;
+    protected PrintStream printStream;
+    protected ScanWrap scanner;
 
-    public static void main(String[] args){}
 
-    protected void start(PrintStream printStream, List<Book> list){
-        welcome(printStream);
-        printMenu(printStream, list);
+    public BibliotecaApp(List<Book> list, PrintStream printStream, ScanWrap scanner){
+        this.list = list;
+        this.printStream = printStream;
+        this.scanner = scanner;
     }
 
-    protected void printMenu(PrintStream printStream, List<Book> list){
-        printStream.println("MENU\nOption 1: List of Books");
+    protected void start(){
+        welcome();
+        printMenu();
+        userChooseOption(scanner);
     }
 
-    protected void welcome(PrintStream printStream) {
+    protected void printMenu(){
+        printStream.println("MENU\nOption 1");
+    }
+
+    protected void welcome() {
         printStream.println("welcome:)");
     }
 
-    protected void listBooks(PrintStream printStream, List<Book> testList) {
-        for (Book i: testList) {
+    protected void listBooks() {
+        for (Book i: list) {
             printStream.println(i);
         }
     }
 
-    public void userChooseOption() {
+    protected void userChooseOption(ScanWrap scanner) {
+        if (scanner.scanInput().equals("Option 1")){
+            listBooks();
+        }
+        else if(scanner.scanInput().equals("Option 2")){
+            checkOut();
+        }/*
+        else if (scanner.scanInput().equals("X")){
+            quitApp();
+        }*/
+        else {
+            printStream.println("Invalid Option!");
+        }
+    }
+
+    protected Boolean isBookInLib(){
+        for (Book i: list){
+            if (scanner.scanInput().equals(i.title)){
+                printStream.println("Thank you! Enjoy the book");
+                return true;
+            }
+        }
+        printStream.println("Sorry, that book is not available");
+        return false;
+    }
+
+    protected Book checkOut(){
+        Book checkedOutBook = null;
+        if (isBookInLib().equals(true)){
+             for(Book i: list) {
+                 if (scanner.scanInput().equals(i.title)) {
+                     checkedOutBook = i;
+                 }
+             }
+
+        }
+        list.remove(checkedOutBook);
+        return checkedOutBook;
+    }
+
+    /*protected void quitApp(){
+
+    }
+
+    public static void main(String[] args){
+
     }
 
     /*public List<Book> addList(Book book){
--- test/com/twu/biblioteca/BibliotecaAppTest.java
+++ test/com/twu/biblioteca/BibliotecaAppTest.java
@@ -4,15 +4,22 @@ package com.twu.biblioteca;
 import com.twu.biblioteca.BibliotecaApp;
 import org.junit.*;
 
+import java.awt.*;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintStream;
 import java.util.*;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 /*
  * Author: Veronica Blackwell
@@ -24,6 +31,7 @@ public class BibliotecaAppTest {
     protected BibliotecaApp biblibtest;
     protected ByteArrayOutputStream outputStream;
     protected PrintStream printStream;
+    protected ScanWrap mockScanner;
     protected List<Book> testList = new ArrayList<Book>();
     protected Book testBook1;
     protected Book testBook2;
@@ -31,39 +39,41 @@ public class BibliotecaAppTest {
 
     @Before
     public void setUp(){
-        biblibtest = new BibliotecaApp();
         outputStream = new ByteArrayOutputStream();
         printStream = new PrintStream(outputStream);
+        mockScanner = mock(ScanWrap.class);
         testBook1 = new Book("It", "Stephan King", 1989);
         testBook2 = new Book("Harry Potter", "JK Rowling", 1991);
         testBook3 = new Book("Corazon", "Yesika S.", 2017);
         testList.add(testBook1);
         testList.add(testBook2);
         testList.add(testBook3);
+        biblibtest = new BibliotecaApp(testList, printStream, mockScanner);
     }
 
     @Test
     public void shouldWelcome() {
-        biblibtest.welcome(printStream);
+        biblibtest.welcome();
         assertThat(outputStream.toString(), is("welcome:)\n"));
     }
 
     @Test
     public void shouldListBooks() {
-        biblibtest.listBooks(printStream, testList);
+        biblibtest.listBooks();
         assertThat(outputStream.toString(), is("It, Stephan King, 1989\nHarry Potter, JK Rowling, 1991\nCorazon, " +
                 "Yesika S., 2017\n"));
     }
 
     @Test
     public void shouldStartMenu(){
-        biblibtest.start(printStream, testList);
-        assertThat(outputStream.toString(), is("welcome:)\nMENU\nOption 1: List of Books\n"));
+        biblibtest.start();
+        assertThat(outputStream.toString(), is("welcome:)\nMENU\nOption 1\n"));
     }
 
     @Test
     public void userShouldChooseOption(){
-        biblibtest.userChooseOption();
+        biblibtest.userChooseOption(mockScanner);
+        assertThat(outputStream.toString(), is("Option 1: List of Books\n"));
     }
 
 }
